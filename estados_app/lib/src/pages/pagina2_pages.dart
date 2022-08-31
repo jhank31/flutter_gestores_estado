0fc9b0f83fa6738b6bdcdbf8defbc4bc07bb8cb3
Jhancarlos Lenis <jhanklear31@gmail.com>
2022-08-31 09:45:22 -0500

    diseño listo

---
+++ estados_app/lib/src/pages/pagina2_pages.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Pagina2Page extends StatelessWidget {
+  const Pagina2Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pagina 2'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: const Text(
+                'Establecer usuario',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: const Text(
+                'Cambiar edad',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: const Text(
+                'Añadir profesion',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
